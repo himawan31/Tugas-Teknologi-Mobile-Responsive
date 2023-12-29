31161296c1f1b0906ce887200814a725ced09dc5
himawan31 <you@example.com>
2023-12-29 20:38:03 +0700

    Update Code

--- app/src/main/java/com/himawankurniaelisanto_5210411325/tugasteknologimobileresponsive/ProfileCompanyActivity.kt
+++ app/src/main/java/com/himawankurniaelisanto_5210411325/tugasteknologimobileresponsive/ProfileCompanyActivity.kt
@@ -1,11 +1,34 @@
 package com.himawankurniaelisanto_5210411325.tugasteknologimobileresponsive
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.himawankurniaelisanto_5210411325.tugasteknologimobileresponsive.databinding.ActivityProfileCompanyBinding
 
+private lateinit var binding: ActivityProfileCompanyBinding
 class ProfileCompanyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_company)
+        binding = ActivityProfileCompanyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.location.setOnClickListener {
+            openGoogleMapsToIndofoodLocation()
+        }
+    }
+
+    private fun openGoogleMapsToIndofoodLocation() {
+        val indofoodAddress = "Jl. Ipda Tut Harsono No.50-52, Muja Muju, Kec. Umbulharjo, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55165"
+        val uri = Uri.parse("geo:0,0?q=$indofoodAddress")
+
+        val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+
+        if (mapIntent.resolveActivity(packageManager) != null) {
+            startActivity(mapIntent)
+        } else {
+
+        }
     }
 }
